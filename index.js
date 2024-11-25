b35fab2bbb81c23a774ad4f89693cc2fb0146c2f
justinjensen <justinjensen@users.noreply.github.com>
2024-11-24 17:03:13 -0700

    exercise 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 
 const PORT = 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("body", (req) => {
